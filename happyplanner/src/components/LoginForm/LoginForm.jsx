21e3c2ad42fa589c3757254fbdc1f520b8f84ae9
Candelaria Ferrari <ferrari.candelaria@gmail.com>
2025-02-25 22:48:38 -0300

    prueba uno de login

--- happyplanner/src/components/LoginForm/LoginForm.jsx
+++ happyplanner/src/components/LoginForm/LoginForm.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import './LoginForm.css';
+//icons
+import { CiUser } from "react-icons/ci";
+import { IoLockClosedOutline } from "react-icons/io5";
+
 const LoginForm = () =>{
   return ( 
     <div className="wrapper">
@@ -7,9 +11,12 @@ const LoginForm = () =>{
         <h1>Login</h1>
         <div className="input-box">
           <input type="text" placeholder="Username" required/>
+          <CiUser className="icon"/>
         </div>
         <div className="input-box">
+       
           <input type="password" placeholder="Password" required/>
+          <IoLockClosedOutline className="icon"/>
         </div>
         <div className="remember-forgot">
           <label>
